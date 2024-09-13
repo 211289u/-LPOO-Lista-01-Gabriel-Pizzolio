b33a17d9a7912d49e0d7c7f20f30d71eb1c64833
211289u <gabrieow1337@gmail.com>
2024-09-12 22:04:22 -0400

    exercicio 07

--- Main.java
+++ Main.java
@@ -1,10 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         Carro meuCarro = new Carro("Toyota", "Corolla", 1983);
+        Moto minhaMoto = new Moto("Honda", "CB 500", 2021, 250);
+
+        System.out.println("Informações antes do abastecimento e aceleração do carro:");
+        meuCarro.exibirInformacoes();
+        meuCarro.abastecer();
+        meuCarro.acelerar(80);
+        System.out.println("Informações atualizadas do carro:");
         meuCarro.exibirInformacoes();
         System.out.println();
 
-        Moto minhaMoto = new Moto("Honda", "CB 500", 2021, 250);
+        System.out.println("Informações antes do abastecimento e aceleração da moto:");
+        minhaMoto.exibirInformacoes();
+        minhaMoto.abastecer();
+        minhaMoto.acelerar(100);
+        System.out.println("Informações atualizadas da moto:");
         minhaMoto.exibirInformacoes();
         System.out.println();
 
--- Veiculo.java
+++ Veiculo.java
@@ -1,4 +1,4 @@
-public abstract class Veiculo {
+public abstract class Veiculo implements IFuncoesVeiculo {
     protected String tipo;
     protected boolean abastecido = false;
     protected int velocidade = 0;
@@ -10,11 +10,13 @@ public abstract class Veiculo {
         System.out.println("Tipo: " + tipo);
     }
 
+    @Override
     public void abastecer() {
         this.abastecido = true;
         System.out.println("O veículo foi abastecido.");
     }
 
+    @Override
     public void acelerar(int velocidade) {
         this.velocidade = velocidade;
         System.out.println("A velocidade do veículo foi ajustada para " + velocidade + " km/h.");
