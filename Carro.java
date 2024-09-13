872e864168ece265217b8f4b4ec16dfdefa6a31f
211289u <gabrieow1337@gmail.com>
2024-09-12 21:49:26 -0400

    exercicio 5

--- Carro.java
+++ Carro.java
@@ -1,3 +1,15 @@
+public abstract class Veiculo {
+    protected String tipo;
+    protected boolean abastecido = false;
+    protected int velocidade = 0;
+
+    public void exibirTipo() {
+        System.out.println("Tipo: " + tipo);
+    }
+
+    public abstract void ligar();
+}
+
 public class Carro extends Veiculo {
     private String marca;
     private String modelo;
@@ -45,5 +57,54 @@ public class Carro extends Veiculo {
     @Override
     public void ligar() {
         System.out.println("O carro está ligado");
+    }
+}
+
+public class Moto extends Veiculo {
+    private String marca;
+    private String modelo;
+    private int ano;
+    private int cilindradas;
+
+    public Moto(String marca, String modelo, int ano, int cilindradas) {
+        super();
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.cilindradas = cilindradas;
+        this.tipo = "Moto";
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano: " + ano);
+        System.out.println("Cilindradas: " + cilindradas);
+    }
+
+    @Override
+    public void ligar() {
+        System.out.println("A moto está ligada");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Carro meuCarro = new Carro("Toyota", "Corolla", 1983);
+        meuCarro.exibirInformacoes();
+        System.out.println();
+
+        Moto minhaMoto = new Moto("Honda", "CB 500", 2021, 250);
+        minhaMoto.exibirInformacoes();
+        System.out.println();
+
+        Veiculo[] veiculos = new Veiculo[2];
+        veiculos[0] = meuCarro;
+        veiculos[1] = minhaMoto;
+
+        for (Veiculo veiculo : veiculos) {
+            veiculo.ligar();
+        }
     }
 }
